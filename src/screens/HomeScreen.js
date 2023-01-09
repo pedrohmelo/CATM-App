1e8899672c8bed73e916b31d2887e3c758c594c5
pedrohmelo <phmc_1995@hotmail.com>
2023-01-09 12:35:16 -0300

    started TCC!

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,40 @@
-import * as React from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 export default function HomeScreen({navigation}) {
   return (
     <View style={styles.container}>
-      <Text
-        onPress={() => navigate}
-      >HOME SCREEN</Text>
+      <View style={styles.userHeader}>
+        <Text>Olá,</Text>
+        <Text>Pedro</Text>
+      </View>
+
+      <ScrollView
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        style={styles.functionalities}
+      >
+        <TouchableOpacity style={styles.buttonAction}>
+          <View style={styles.buttonArea}>
+            <Image source={require('../assets/images/todolist.png')} style={styles.imageLogo} />
+            <Text>teste</Text>
+          </View>          
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonAction}>
+          <View style={styles.buttonArea}>
+            <Image source={require('../assets/images/todolist.png')} style={styles.imageLogo} />
+            <Text>teste</Text>
+          </View>          
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.buttonAction}>
+          <View style={styles.buttonArea}>
+            <Image source={require('../assets/images/todolist.png')} style={styles.imageLogo} />
+            <Text>teste</Text>
+          </View>          
+        </TouchableOpacity>
+      </ScrollView>
     </View>
   );
 }
@@ -15,7 +43,32 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  userHeader: {
+    paddingHorizontal: 15
+  },
+  functionalities: {
+    alignSelf: 'center',
+    maxHeight: 120,
+    marginBottom: 14,
+    marginTop: '5%',
+    paddingEnd: 14,
+    paddingStart: 14,
+  },
+  buttonAction: {
+    alignItems: 'center',
+    marginRight: 15
+  },
+  buttonArea: {
+    backgroundColor: '#ecf0f1',
+    height: 110,
+    width: 100,
+    borderRadius: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageLogo: {
+    flex: 1,
+    resizeMode: 'contain'
+  }
 });

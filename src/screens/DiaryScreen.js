b8a4a3b264985115db22f8d396e201e76e611d8c
pedrohmelo <phmc_1995@hotmail.com>
2023-01-29 15:55:28 -0300

    initiated calendar screen

--- src/screens/DiaryScreen.js
+++ src/screens/DiaryScreen.js
@@ -1,17 +1,147 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function DiaryScreen({navigation}) {
   return (
-    <View style={styles.container}>
-      <Text>Calendário aqui</Text>
+    <View style={styles.month}>
+      <View style={styles.week}>
+        <TouchableOpacity style={styles.day}>
+          <Text>01</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>02</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>03</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>04</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>05</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>06</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>07</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.week}>
+        <TouchableOpacity style={styles.day}>
+          <Text>01</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>02</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>03</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>04</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>05</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>06</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>07</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.week}>
+        <TouchableOpacity style={styles.day}>
+          <Text>01</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>02</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>03</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>04</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>05</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>06</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>07</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.week}>
+        <TouchableOpacity style={styles.day}>
+          <Text>01</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>02</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>03</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>04</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>05</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>06</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity style={styles.day}>
+          <Text>07</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  month: {},
+  week: {
     backgroundColor: '#fff',
+    flexDirection: 'row',
+    alignItems: 'center',
   },
+  day: {
+    borderWidth: 2,
+    borderColor: 'green',
+    width: 38,
+    height: 38,
+    borderRadius: 19,
+    backgroundColor: 'orange',
+    marginHorizontal: 5,
+    marginVertical: 5
+  }
 }); 

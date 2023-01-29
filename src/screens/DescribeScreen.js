77bcf5911a6a943b1026c20ffdcdff60ee4fb334
pedrohmelo <phmc_1995@hotmail.com>
2023-01-29 17:03:05 -0300

    removed scrollview from describeScreen

--- src/screens/DescribeScreen.js
+++ src/screens/DescribeScreen.js
@@ -1,19 +1,64 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function DescribeScreen() {
   return (
     <View style={styles.container}>
-      <Text>DIARY DESCRIBE</Text>
+      <View>
+        <Text>Como você está hoje?</Text>
+
+          <View style={styles.emojisBox}>
+            <TouchableOpacity style={styles.buttonAction}>
+              <View style={styles.buttonArea}>
+                <Text style={styles.emoji}>😄</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonAction}>
+              <View style={styles.buttonArea}>
+                <Text style={styles.emoji}>😄</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonAction}>
+              <View style={styles.buttonArea}>
+                <Text style={styles.emoji}>😄</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity style={styles.buttonAction}>
+              <View style={styles.buttonArea}>
+                <Text style={styles.emoji}>😄</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
+  emojisBox: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  buttonAction: {
+    marginRight: 10
+  },
+  buttonArea: {
+    backgroundColor: '#ecf0f1',
+    height: 80,
+    width: 80,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  emoji: {
+    fontSize: 40
+  }
 });

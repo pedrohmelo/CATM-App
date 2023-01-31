1a23d5b24ffe1ffc5ca0e9132f5778b381b8a047
pedrohmelo <phmc_1995@hotmail.com>
2023-01-31 14:52:36 -0300

    adjusts

--- src/screens/DescribeScreen.js
+++ src/screens/DescribeScreen.js
@@ -16,7 +16,7 @@ export default function DescribeScreen() {
   return (
     <View style={styles.container}>
       <View>
-        <Text>Como você está hoje?</Text>
+        <Text style={styles.title}>Como você está hoje?</Text>
 
           <View style={styles.emojisBox}>
             <TouchableOpacity 
@@ -58,18 +58,19 @@ export default function DescribeScreen() {
           
       </View>
       <View>
-        <Text style={styles.textBoxTitle}>AAAA</Text>
+        <Text style={styles.textBoxTitle}>Descreva um pouco blablabla</Text>
         <TextInput
           style={styles.textBox}
           placeholder='teste'
           placeholderTextColor='blue'
+          multiline={true}
           //value={text} TEM QUE SALVAR ISSO AQUI NUMA VARIÁVEL DEPOIS
         />
       </View>
 
       <View>
-        <TouchableOpacity>
-          <Text>SEND</Text>
+        <TouchableOpacity style={styles.saveButton}>
+          <Text style={styles.saveButtonText}>Salvar</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -80,8 +81,12 @@ const styles = StyleSheet.create({
   container: {
     
   },
+  title: {
+    textAlign: 'center',
+    fontWeight: 'bold'
+  },
   emojisBox: {
-    backgroundColor: 'red',
+    marginTop: 10,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center'
@@ -95,9 +100,9 @@ const styles = StyleSheet.create({
   },
   buttonArea: {
     backgroundColor: '#ecf0f1',
-    height: 80,
-    width: 80,
-    borderRadius: 25,
+    height: 70,
+    width: 70,
+    borderRadius: 35,
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -107,12 +112,27 @@ const styles = StyleSheet.create({
   textBoxTitle: {
     fontSize: 15,
     fontWeight: 'bold',
-    textAlign: 'center'
+    textAlign: 'center',
+    marginBottom: 10
   },
   textBox: {
     width: '90%',
+    //height: 350,
     alignSelf: 'center',
     borderWidth: 2,
-    borderRadius: 5
+    borderRadius: 5,
+    //backgroundColor: 'blue'
+  },
+  saveButton: {
+    marginTop: 10,
+    width: '50%',
+    borderWidth: 1,
+    borderColor: 'orange',
+    alignSelf: 'center',
+    backgroundColor: 'green'
+  },
+  saveButtonText: {
+    textAlign: 'center',
+
   }
 });

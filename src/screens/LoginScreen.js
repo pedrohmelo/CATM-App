31e6bd6ecedb2f13bf5db402b6b5f452d4b6ee0d
pedrohmelo <phmc_1995@hotmail.com>
2023-02-07 13:59:55 -0300

    adjusts 2

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext } from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Image } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, Image, TouchableOpacity } from 'react-native';
 
 import InputFields from '../components/InputFields';
 import InputPasswordFields from '../components/InputPasswordFields';
@@ -20,27 +20,50 @@ export default function LoginScreen() {
         <Text style={styles.loginText}>Entre para continuar</Text>
       </View>
 
-        <View style={styles.fields}>
-          <InputFields
-            placeholder='Digite seu e-mail'
-            content={email}
-            setContent={setEmail}
-          />
+      <View style={styles.fields}>
+        <InputFields
+          placeholder='Digite seu e-mail'
+          content={email}
+          setContent={setEmail}
+        />
 
-          <InputPasswordFields
-            placeholder='Digite sua senha'
-            content={password}
-            setContent={setPassword}
-          />
-        </View>
+        <InputPasswordFields
+          placeholder='Digite sua senha'
+          content={password}
+          setContent={setPassword}
+        />
+      </View>
+
+      <TouchableOpacity
+        //onPress={() => navigation.navigate('RecoverStack')}
+        style={styles.forgotPassword}
+      >
+        <Text style={styles.forgotPasswordText}>Esqueceu sua senha?</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={[styles.loginButton, { backgroundColor: (email && password) ? '#CBD7CD' : '#EDF1F3' }]}
+        disabled={!(email && password)}
+        //onPress={() => handleLogin()}
+      >
+        <Text style={[styles.loginButtonText, { color: (email && password) ? '#1D1F1D' : '#ACB5BB' }]}>Entrar</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.signInButton}
+        //onPress={() => navigation.navigate('SignInStack')}
+      >
+        <Text style={styles.signInButtonText}>Criar Conta</Text>
+      </TouchableOpacity>
+
     </KeyboardAvoidingView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
-    
+    flex: 1,
+    paddingHorizontal: '5%'
   },
   logoBox: {
     alignItems: 'center',
@@ -66,5 +89,39 @@ const styles = StyleSheet.create({
     color: '#6D6D6D',
     fontSize: 16,
     fontWeight: 'bold',
+  },
+  forgotPassword: {
+    width: '100%',
+    alignItems: 'flex-end',
+  },
+  forgotPasswordText: {
+    fontWeight: 'bold',
+    color: '#1D1F1D',
+    fontSize: 14
+  },
+  loginButton: {
+    width: '100%',
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: '15%'
+  },
+  loginButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  signInButton: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 20,
+    marginTop: '5%'
+  },
+  signInButtonText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#1D1F1D',
   },
 });
